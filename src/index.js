ada75064424c3d6e482e6349af921eea991baa47
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-22 16:40:36 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
   let form=document.querySelector('form')
   form.addEventListener('submit', (e)=>{
     e.preventDefault()
-    taskDescription(e.target.new-task-description.value)
+  
+    taskDescription(e.target['new-task-description'].value)
     form.reset();
   })
-});
-function taskDescription(task){
-  let p=document.createElement('p')
-  let btn=document.createElement('button')
-  btn.addEventListener('click',deleteElement)
-  btn.textContent='x'
-  p.textContent=`${todo}`
-  p.appendChild(btn)
-  console.log(p)
-  document.querySelector('#list').appendChild(p)
-}
-function deleteElement(e){
-  e.target.parentNode.remove()
-}
+  function taskDescription(task){
+    let p=document.createElement('p')
+    let btn=document.createElement('button')
+    btn.addEventListener('click',deleteElement)
+    btn.textContent='x'
+    p.textContent=`${task}`
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector('#list').appendChild(p)
+  };
+  function deleteElement(e){
+    e.target.parentNode.remove()
+  }
+  
+})
+
+
